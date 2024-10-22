18ba19a9cdf9faded48b377de8da8bcbee07e91d
Victor Hugo Cruz <hugov4308@gmail.com>
2024-10-22 11:50:50 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -38,10 +38,10 @@ sudo gpasswd -a $USER docker
 
 # Install VS Code
 echo "Installing Visual Studio Code..."
-sudo apt install software-properties-common apt-transport-https wget -y
+sudo apt install -y software-properties-common apt-transport-https wget 
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-sudo apt install code -y
+sudo apt install -y code 
 
 # Install AWS CLI
 echo "Installing AWS CLI..."
